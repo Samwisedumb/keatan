e0e94ed14d9efa6773a7645586a3d4ed586e61d2
Michael Carson Rawlins <mr399@flanders.cs.byu.edu>
2015-11-16 17:10:11 -0700

    Added password class

---
+++ java/src/shared/transferClasses/Password.java
@@ -0,0 +1,38 @@
+package shared.transferClasses;
+
+import shared.exceptions.InvalidObjectException;
+
+/**
+ * A dumb data container for the user's password.<br>
+ * <h6>Validate</h6><ul>
+ * <li>A password must be at least 5 characters long</li>
+ * <li>A password may contain only letters, digits, underscores, and hyphens</li>
+ * </ul>
+ * @author mr399
+ */
+public class Password implements ITransferClass {
+	private String password;
+	
+	public Password() {}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public void validate() throws InvalidObjectException {
+		if (password == null) {
+			throw new InvalidObjectException("The password was null.");
+		}
+		if (password.length() < 5) {
+			throw new InvalidObjectException("The password is shorter than 5 characters.");
+		}
+		for (char c : password.toCharArray()) {
+            if (!Character.isLetterOrDigit(c) && c != '_' && c != '-') {
+    			throw new InvalidObjectException("The password contains invalid characters.");
+            }
+        }
+	}
+}

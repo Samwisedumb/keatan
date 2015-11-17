f6be2ee6701ce8ef42465f925f928bb557cf2eec
Michael Carson Rawlins <mr399@flanders.cs.byu.edu>
2015-11-16 17:35:30 -0700

    Added hashcode and equals methods

--- java/src/shared/transferClasses/UserCredentials.java
+++ java/src/shared/transferClasses/UserCredentials.java
@@ -53,4 +53,37 @@ public class UserCredentials {
 		username.validate();
 		password.validate();
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserCredentials other = (UserCredentials) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 }

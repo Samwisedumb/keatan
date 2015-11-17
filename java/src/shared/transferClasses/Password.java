542b58f8c8e0a2697017602a4fb9ce6654e13cab
Joshua Curtis Dawson <djoshuac@grampa.cs.byu.edu>
2015-11-16 19:45:36 -0700

    equals hashCode in Password, removed WHATNO file

--- java/src/shared/transferClasses/Password.java
+++ java/src/shared/transferClasses/Password.java
@@ -41,4 +41,30 @@ public class Password implements ITransferClass {
 	public String toString() {
 		return password;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Password other = (Password) obj;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
 }

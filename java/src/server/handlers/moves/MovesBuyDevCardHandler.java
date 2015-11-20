8a102e44b0794033a0328d9c99d8e882dacb9ed5
ViscountPudding <stephenmccammon@comcast.net>
2015-11-20 14:41:06 -0700

    blergh

---
+++ java/src/server/handlers/moves/MovesBuyDevCardHandler.java
@@ -0,0 +1,17 @@
+package server.handlers.moves;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import server.handlers.IHandler;
+
+public class MovesBuyDevCardHandler extends IHandler {
+
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
